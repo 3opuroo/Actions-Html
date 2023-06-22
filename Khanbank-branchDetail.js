51604abc2c2525e4f9ed2cd5ff735006cb3e0789
3opuroo <109808715+3opuroo@users.noreply.github.com>
2023-06-22 17:04:19 +0800

    Update Khanbank-branchDetail.js

--- Khanbank-branchDetail.js
+++ Khanbank-branchDetail.js
@@ -21,8 +21,8 @@ const executeCode = async () => {
     let sunday = "Ням";
     let phone = "Утас";
     let timetable = "Цагийн хуваарь";
-    let _title = "Буцах"
-    let weekend = "Амарна"
+    let _title = "Буцах";
+    let weekend = "Амарна";
 
     let numToEmoji = (num) => {
       if (num === "1") return "✅";
@@ -30,8 +30,9 @@ const executeCode = async () => {
     };
 
     let timeToText = (n1, n2, n3, n4) => {
-      if (n1 === "00:00" && n2 === "00:00" && n3 === "00:00" && n4 === "00:00") return weekend;
-      else return ""
+      if (n1 === "00:00" && n2 === "00:00" && n3 === "00:00" && n4 === "00:00")
+        return weekend;
+      else return "";
     };
 
     if (param.lang === "en") {
@@ -50,49 +51,81 @@ const executeCode = async () => {
       sunday = "Sun";
       phone = "Phone";
       timetable = "Timetable";
-      _title = 'Back'
-      weekend = "Closed"
+      _title = "Back";
+      weekend = "Closed";
     }
 
-    let text1
-    if(param.type === "atm" || param.type === "express") {
+    let text1;
+    if (param.type === "atm" || param.type === "express") {
       text1 = `${address}\n\n`;
-    text1 += `${phone}: ${param.phone}\n\n`;
-    text1 += `${param.timeTable.replace(/(<([^>]+)>)/gi, "")}`;
-    } else if(param.type === "kiosk"){
+      text1 += `${phone}: ${param.phone}\n\n`;
+      text1 += `${param.timeTable.replace(/(<([^>]+)>)/gi, "")}`;
+    } else if (param.type === "kiosk") {
       text1 = `${address}\n\n`;
-    text1 += `${phone}: ${param.phone}\n\n`;
-    text1 += `${monday} - ${sunday}: ${param.mondayStart} - ${param.sundayEnd}\n`;
+      text1 += `${phone}: ${param.phone}\n\n`;
+      text1 += `${monday} - ${sunday}: ${param.mondayStart} - ${param.sundayEnd}\n`;
     } else {
-    text1 = `${address}\n\n`;
-    text1 += `ATM: ${numToEmoji(param.atm)}\n`;
-    text1 += `Express: ${numToEmoji(param.express)}\n`;
-    text1 += `Kiosk: ${numToEmoji(param.kiosk)}\n`;
-    text1 += `${wheelchair}: ${numToEmoji(param.wheelchair)}\n`;
-    text1 += `${currency}: ${numToEmoji(param.currency)}\n`;
-    text1 += `${card}: ${numToEmoji(param.card)}\n`;
-    text1 += `${international}: ${numToEmoji(param.international)}\n`;
-    text1 += `${saving}: ${numToEmoji(param.saving)}\n\n`;
-    text1 += `${phone}: ${param.phone}\n\n`;
-    text1 += `${monday} - ${friday}: ${param.mondayStart} - ${param.fridayEnd}\n`;
-    text1 += `${saturday}, ${sunday}: ${timeToText(param.saturdayStart, param.saturdayEnd, param.sundayStart, param.sundayEnd)}`;
+      text1 = `${address}\n\n`;
+      text1 += `Express: ${numToEmoji(param.express)}\n`;
+      text1 += `Kiosk: ${numToEmoji(param.kiosk)}\n`;
+      text1 += `${wheelchair}: ${numToEmoji(param.wheelchair)}\n`;
+      text1 += `${phone}: ${param.phone}\n\n`;
+      text1 += `${monday}: ${
+        param.mondayStart === "00:00"
+          ? "Амарна"
+          : `${param.mondayStart} - ${param.mondayEnd}`
+      }\n`;
+      text1 += `${tuesday}: ${
+        param.tuesdayStart === "00:00"
+          ? "Амарна"
+          : `${param.tuesdayStart} - ${param.tuesdayEnd}`
+      }\n`;
+      text1 += `${wednesday}: ${
+        param.wednesdayStart === "00:00"
+          ? "Амарна"
+          : `${param.wednesdayStart} - ${param.wednesdayEnd}`
+      }\n`;
+      text1 += `${thursday}: ${
+        param.thursdayStart === "00:00"
+          ? "Амарна"
+          : `${param.thursdayStart} - ${param.thursdayEnd}`
+      }\n`;
+      text1 += `${friday}: ${
+        param.fridayStart === "00:00"
+          ? "Амарна"
+          : `${param.fridayStart} - ${param.fridayEnd}`
+      }\n`;
+      text1 += `${saturday}: ${
+        param.saturdayStart === "00:00"
+          ? "Амарна"
+          : `${param.saturdayStart} - ${param.saturdayEnd}`
+      }\n`;
+      text1 += `${sunday}: ${
+        param.sundayStart === "00:00"
+          ? "Амарна"
+          : `${param.sundayStart} - ${param.sundayEnd}`
+      }\n`;
     }
     let message2 = {
-        attachment: {
-            type: "template",
-            payload: {
-                template_type: "button",
-                text: text1,
-                buttons: [
-                    {
-                        "title": _title,
-                        "type": "postback",
-                        "payload": `/api/action/call?actionId=312&lang=${param.lang}&currentIndex=${currentIndex - 1}&provinceCode=${param.provinceCode}&type=${param.type}`
-                    },
-                ]
-            }
-        }
-    }
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "button",
+          text: text1,
+          buttons: [
+            {
+              title: _title,
+              type: "postback",
+              payload: `/api/action/call?actionId=322&lang=${
+                param.lang
+              }&currentIndex=${currentIndex - 1}&provinceCode=${
+                param.provinceCode
+              }&type=${param.type}`,
+            },
+          ],
+        },
+      },
+    };
 
     await sendMessage(bot, sender.sender_id, message2);
     //await sendMessage(bot, sender.sender_id, {text: filteredDistrict[0].name});
